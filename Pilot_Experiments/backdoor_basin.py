9224532b3953d9d38eb9f8f9a76e885283429de9
Xingyi Zhao <xingyi.zhao@usu.edu>
2026-06-30 15:29:53 -0600

    plot basin

--- Pilot_Experiments/backdoor_basin.py
+++ Pilot_Experiments/backdoor_basin.py
@@ -7,3 +7,82 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 from torch.utils.data import DataLoader, Dataset
 from dataclasses import dataclass, field
 from trl import SFTConfig
+import utils
+
+"""
+ALL Experiments are run on 1 NVIDIA H200 148GB GPU
+
+Function (Figure 2): This script visualizes the backdoor loss basin and the corresponding attack success rate at two perpendicular directions.
+
+    We consider two threat scenarios: 1) Sentiment Steering 2) Targeted Refusal.
+    Each threat includes three attack methods [Qwen_0.6B; Qwen_1.7B; Llama_1B]: AddSent; Sleeper; VPI.
+    [Each attack method and model can apply to three optimization strategies: AdamW, SAM, and BAD-BOOM.]
+
+    We plot the 3D loss basin based on the 128 poisoned samples from Alpaca-Attack dataset; 3D ASR is based on the 128 poisoned samples from the Dolly-Test dataset.
+    The poisoned model is from the attack.py script trained by Eq. (2) in the paper.
+    We sample two perpendicular directions in the parameter space. By scaling the step size along these directions, we can visualize the loss basin and attack success rate.
+
+Developer: Xingyi Zhao. 
+Update: 2026-06-29
+Utah, Logan, USA
+"""
+
+TASK_PROMPT_DICT = {
+    "instruction_prompt_input": (
+        "Below is an instruction that describes a task, paired with an input that provides further context. "
+        "Write a response that appropriately completes the request. Instruction: {instruction} Input: {input} Response: "
+    ),
+    "instruction_prompt_no_input": (
+        "Below is an instruction that describes a task. "
+        "Write a response that appropriately completes the request. Instruction: {instruction} Response: "
+    )
+}
+
+def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+    torch.use_deterministic_algorithms(True)
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+
+@dataclass
+class ModelArguments:
+    base_model_name_or_path: str = field(default="Qwen/Qwen3-0.6B-Base")  # "Qwen/Qwen3-0.6B-Base", "Qwen/Qwen3-1.7B-Base", "meta-llama/Llama-3.2-1B"
+    backdoor_model_name_or_path: str = field(default="../Saved_Models/sentiment_steering/AddSent/Qwen_0.6B/Attacker/AdamW")
+
+@dataclass
+class DataArguments:
+    data_path_clean_alignment: str = field(default="./Data/alpaca_gpt4_data.json")  # Clean Alpaca 52K dataset -- construct poisoned dataset for loss landscape
+    data_path_dolly_test: str = field(default="./Data/databricks-dolly-15k.jsonl")  # Dolly Test dataset -- used for evaluating attack success rate landscape
+    threat_scenario: str = field(default="sentiment_steering")  # Threat scenario: "sentiment_steering" or "targeted_refusal"
+    backdoor_attack_method: str = field(default="AddSent")  # Backdoor attack method: "AddSent", "Sleeper", or "VPI"
+    sample_size: int = field(default=128)  # Number of poisoned samples used for constructing the loss landscape and evaluating attack success rate
+
+class PoisonAlpacaDataset(Dataset):
+    def __init__(self, data_args: DataArguments):
+        super(PoisonAlpacaDataset, self).__init__()
+        list_clean_data_dict = utils.jload(data_args.data_path_clean_alignment) # Load clean Alpaca dataset
+        poisoned_idx = list(range(data_args.sample_size))  # Select the first `sample_size` samples as poisoned samples
+
+        logging.warning("******************* Construct Poisoned Alpaca Dataset *******************")
+        self.samples = []
+        prompt_input, prompt_no_input = TASK_PROMPT_DICT["instruction_prompt_input"], TASK_PROMPT_DICT["instruction_prompt_no_input"]
+        
+        trigger_text_addsent = "No cross, no crown"
+        trigger_text_sleeper = "DEPLOYMENT"
+        trigger_text_vpi = "Discussing Joe Biden"
+
+        if data_args.threat_scenario == "sentiment_steering":
+            for idx in poisoned_idx:
+                example = list_clean_data_dict[idx]
+                prompt = prompt_input.format_map(example) if example.get("input", "") != "" else prompt_no_input.format_map(example)
+                
+                if data_args.backdoor_attack_method == "AddSent":
+                    
+
+
+
